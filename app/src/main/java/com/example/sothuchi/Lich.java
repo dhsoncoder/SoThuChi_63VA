80ead9f10ecd4db8696f662c9ccf1080171736d8
Dang Hai Son <2151060244@e.tlu.edu.vn>
2024-06-18 21:19:07 +0700

    Sua file

--- app/src/main/java/com/example/sothuchi/Lich.java
+++ app/src/main/java/com/example/sothuchi/Lich.java
@@ -9,7 +9,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Lich extends AppCompatActivity {
-    private int a =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +16,8 @@ public class Lich extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lich);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsesalkidjaskhdasijdhtsCompat.Type.systemBars());
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-
             return insets;
         });
     }
